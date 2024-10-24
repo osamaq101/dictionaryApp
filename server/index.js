e5df02d6b86cf81f9425300d8efdf59cd64a7877
Osama <usamasight@gmail.com>
2024-10-24 12:34:19 -0500

    added environment variable

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dictionaryRoutes from './routes/dictionaryRoutes.js';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
